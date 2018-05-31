e6c7e26c749febc0c5087357965f0d97e19eb2eb
Nelson Lee <nelson@cleverbanana.com>
2018-05-31 10:34:25 -0700

    Change MediaUploader versions

--- app/uploaders/concerns/cb_stem/image_concern.rb
+++ app/uploaders/concerns/cb_stem/image_concern.rb
@@ -9,17 +9,17 @@ module CbStem
       process resize_to_fit: [3000, 3000], if: :image?
 
       # Mobile version
-      version :mobile, if: :image? do
+      version :sm, if: :image? do
         process resize_to_fit: [600, 600]
       end
 
-      # Tablet version
-      version :tablet, if: :image? do
+      # medium version
+      version :md, if: :image? do
         process resize_to_fit: [1200, 1200]
       end
 
       # Desktop version
-      version :desktop, if: :image? do
+      version :lg, if: :image? do
         process resize_to_fit: [2400, 2400]
       end
 
