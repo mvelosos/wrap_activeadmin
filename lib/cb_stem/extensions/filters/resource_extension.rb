fccf5fdd0d25340ae76f4a663a49d12080e20951
Nelson Lee <nelson@cleverbanana.com>
2018-06-25 17:31:07 -0700

    Fix filter close tooltip

--- lib/cb_stem/extensions/filters/resource_extension.rb
+++ lib/cb_stem/extensions/filters/resource_extension.rb
@@ -7,17 +7,21 @@ module ActiveAdmin
 
       private
 
-      # rubocop:disable Metric/MethodLength
+      # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
       def filters_sidebar_section
         ActiveAdmin::SidebarSection.new(
           :filters, only: :index, if: -> { active_admin_config.filters.any? }
         ) do
           header_action do
-            div title: I18n.t('active_admin.filter_close'),
-                'data-toggle': 'tooltip', 'data-placement': 'bottom' do
-              a(i('', class: 'nc-icon nc-simple-remove'),
-                class: 'btn btn-link',
-                target: '_blank', 'data-toggle': 'collapse', 'data-target': '#filters')
+            div do
+              a(class: 'btn btn-link',
+                title: I18n.t('active_admin.filter_close'),
+                target: '_blank', 'data-toggle': 'collapse', 'data-target': '#filters') do
+                div(class: 'tooltip-holder', 'data-toggle': 'tooltip',
+                    'data-placement': 'bottom',
+                    'data-original-title': I18n.t('active_admin.filter_close'))
+                i('', class: 'nc-icon nc-simple-remove')
+              end
             end
           end
           active_admin_filters_form_for assigns[:search], active_admin_config.filters
